7106691d205e3f9eca855ecb02775f443f3ec212
sylvester <nkwontasylvester15@gmail.com>
2022-01-19 14:26:21 -0900

    migrations commit

--- electronics/models.py
+++ electronics/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=250)
+    image=models.ImageField(upload_to='product_images')
+    price=models.FloatField()
+
+    def __str__(self) -> str:
+        return str(self.name)
