9dadb33a9f4cd2c7238ba53e04e7ed73fa2c57cb
sylvester <nkwontasylvester15@gmail.com>
2022-01-19 14:38:08 -0900

    changes made

--- electronics/views.py
+++ electronics/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def list_products(request):
+    products=Product.objects.all()
+    ctx={
+        'products':products
+    }
+    return render(request, 'products.html', ctx)
